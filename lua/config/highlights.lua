9dffaae3049f2a552149223035765ae0869ba656
Owen L <32573897+mageowl@users.noreply.github.com>
2024-08-23 15:04:07 -0700

    highlights

--- lua/config/highlights.lua
+++ lua/config/highlights.lua
@@ -1,9 +1,20 @@
 return {
 	with_palette = function(colors)
 		return {
+			CursorLine = { bg = colors.base },
+
+			Type = { italic = true },
+			Macro = { link = "Function" },
+			-- builtin functions in rust
+			["@lsp.typemod.function.defaultLibrary"] = { link = "@function" },
+			["@lsp.typemod.method.defaultLibrary"] = { link = "@function" },
+			["@lsp.typemod.macro.defaultLibrary"] = { link = "@function" },
+			-- Enums
+			["@lsp.type.enumMember"] = { fg = colors.teal },
+
 			-- Telescope
 			TelescopeTitle = { fg = colors.green },
-			FloatBorder = { fg = colors.surface1 },
+			TelescopeBorder = { fg = colors.surface1 },
 		}
 	end
 }
